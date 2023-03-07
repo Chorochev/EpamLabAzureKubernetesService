5175e62860d04f93ba1a3a19958c3df31cd2af0d
chorochev <chorochev@ya.ru>
2023-03-07 21:54:09 +0600

    create pods with yaml

---
+++ locallab/commands.ps1
@@ -0,0 +1,25 @@
+kubectl get nodes
+kubectl get pods
+
+#########################################################
+# create the pod - httpd
+kubectl apply -f web_pod.yaml
+
+kubectl describe pods web-app
+
+kubectl port-forward web-app 7000:80
+# http://localhost:7000
+
+kubectl delete pods web-app
+
+#########################################################
+# create the pod - chorochev/simple-python-app-service
+# !!!! --port=5000
+kubectl apply -f pythonapp_pod.yaml
+
+kubectl describe pods pythonapp
+
+kubectl port-forward pythonapp 9000:5000
+# http://localhost:9000
+
+kubectl delete pods pythonapp
