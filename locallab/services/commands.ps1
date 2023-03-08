a1e6d35d86bd709925a9de6e6ee0a5c5ca409379
chorochev <chorochev@ya.ru>
2023-03-08 13:01:38 +0600

    create LoadBalancer services

--- locallab/services/commands.ps1
+++ locallab/services/commands.ps1
@@ -4,6 +4,12 @@ kubectl apply -f web_loadbalancer_service.yaml
 
 # kubectl delete -f web_loadbalancer_service.yaml
 
+#########################################################
+# create LoadBalancer services (the deployment - httpd, pythonapp)
+kubectl apply -f multi_loadbalancer_service.yaml
+
+# kubectl delete -f multi_loadbalancer_service.yaml
+
 # checking
 kubectl get nodes
 kubectl get pods
