01881f552ccb5eff07e8b22d24379ad6a504460d
chorochev <chorochev@ya.ru>
2023-03-08 13:21:43 +0600

    create an autoscaling service

--- locallab/services/commands.ps1
+++ locallab/services/commands.ps1
@@ -10,8 +10,18 @@ kubectl apply -f multi_loadbalancer_service.yaml
 
 # kubectl delete -f multi_loadbalancer_service.yaml
 
+
+#########################################################
+# create HorizontalPodAutoscaler services (the deployment - httpd)
+kubectl apply -f web_autoscaling_service.yaml
+
+# kubectl delete -f web_autoscaling_service.yaml
+
 # checking
 kubectl get nodes
 kubectl get pods
 kubectl get deploy
 kubectl get svc
+
+# go to the internal net
+minikube ssh
