35e4fef2df55837297e3dbb8dabb8be8fc3c80c5
chorochev <chorochev@ya.ru>
2023-03-09 10:50:01 +0600

    create docker image

--- docker/comands.ps1
+++ docker/comands.ps1
@@ -14,3 +14,8 @@ docker run --publish 8080:5000 python-docker
 
 # Stop a docker container
 docker stop python-docker
+
+# Create a tag for the image
+docker tag python-docker chorochev/simple-python-app-service:latest
+# Download the image to DockerHub
+docker push chorochev/simple-python-app-service:latest
