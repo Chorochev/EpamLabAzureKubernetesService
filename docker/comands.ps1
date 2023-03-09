2110b93e4cbe426e68d8058ca037de53ab23c7a1
chorochev <chorochev@ya.ru>
2023-03-09 14:15:19 +0600

    add redis

--- docker/comands.ps1
+++ docker/comands.ps1
@@ -1,7 +1,12 @@
-# create requirements.txt
 # go to ..\docker\python
+pip install flask
+pip install redis
+# run python app
+python3 -m flask run
+# create requirements.txt
 python3 -m pip freeze > requirements.txt
 
+######################################################
 # Build an image
 # go to ..\docker
 docker build --file DockerFile --tag python-docker .
@@ -15,7 +20,12 @@ docker run --publish 8080:5000 python-docker
 # Stop a docker container
 docker stop python-docker
 
+# update version
+docker tag chorochev/simple-python-app-service:latest chorochev/simple-python-app-service:v1
+docker push chorochev/simple-python-app-service:v1
+
 # Create a tag for the image
 docker tag python-docker chorochev/simple-python-app-service:latest
+
 # Download the image to DockerHub
 docker push chorochev/simple-python-app-service:latest
--- docker/python/app.py
+++ docker/python/app.py
@@ -1,22 +1,62 @@
 import socket
 from flask import Flask
+from redis import Redis
+import os
+
 app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
+
 
 @app.route('/')
-def hello_world():   
-    try:    
-        ## getting the hostname by socket.gethostname() method
-        hostname = socket.gethostname()
-        ## getting the IP address using socket.gethostbyname() method
-        ip_address = socket.gethostbyname(hostname)
+def hello():
+    try:
         body = "<!DOCTYPE html><html><head><title>EpamLabKubernetes - Python AppService.</title></head><body>"
-        body += "<h1><p style='color:rgb(25,200,25);'>Python AppService</p></h1>" 
-        body += "<h2><p>Aleksei Khoroshev</p></h2>"        
-        body += "<div>Hello World</div>"        
-        body += "<div>Hostname: " + str(hostname) + "</br>IP Address: " + str(ip_address) + "</div>"
-        body += "</body></html>"        
-        return body.encode()
-    except Exception as e:        
+        body += "<h1><p style='color:rgb(25,200,25);'>Python AppService</p></h1>"
+        body += "<h2><p>Aleksei Khoroshev</p></h2>"
+        body += "<div>Epam Lab</div>"
+        body += "<div>"
+        body += get_hostname_div()
+        body += "</div>"
+        body += "<div>"
+        body += get_redis_div()
+        body += "</div>"
+        body += "</body></html>"
+        return body
+    except Exception as e:
         body = "<h1><p style='color:rgb(255,0,0);'>Python AppService Error</p></h1></br>"
         body += "<div>" + str(e) + "</div>"
-        return body.encode()
+        return body
+
+
+def get_hostname_div():
+    try:
+        # getting the hostname by socket.gethostname() method
+        hostname = socket.gethostname()
+        # getting the IP address using socket.gethostbyname() method
+        ip_address = socket.gethostbyname(hostname)
+        body = "<div>"
+        body += "Hostname: " + str(hostname)
+        body += "</br>"
+        body += "IP Address: " + str(ip_address)
+        body += "</div>"
+        return body
+    except Exception as e:
+        body = "<h1><p style='color:rgb(255,0,0);'>Hostname Error</p></h1></br>"
+        body += "<div>" + str(e) + "</div>"
+        return body
+
+
+def get_redis_div():
+    try:
+        name = os.environ.get('NAME')
+        redis.incr('hits')
+        strRedis = 'Greetings from ' + name + \
+            ' %s times!!!' % redis.get('hits')
+        body = "<div>"
+        body += str(strRedis)
+        body += "</div>"
+        return body
+    except Exception as e:
+        body = "<h3><p style='color:rgb(255,0,0);'>Redis Error</p></h3></br>"
+        body += "<div>" + str(e) + "</div>"
+        return body
